fc161eac6d36dd3bb8dfcb52f493c87c6f4196bd
Adam Walker <adamw207@yahoo.com>
2021-02-09 17:17:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,45 @@
-# Add your code here
+#class Dog
+
+ # attr_accessor :name
+
+#  @@all = []
+
+ # def initialize(name)
+ #   @name = name
+ #   @@all << self
+ # end
+
+#  def self.all
+#    @@all.each do |dog|
+#      puts dog.name
+#    end
+#  end
+
+#  def self.clear_all
+#    @@all.clear
+#  end
+
+#end
+
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
